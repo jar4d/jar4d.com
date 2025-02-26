044881431963c874d0a0c6b81260da845a46ab30
jar4d <jared.ruddy@gmail.com>
2025-02-26 14:20:45 +0000

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import image from "@astrojs/image";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,12 +8,17 @@ export default defineConfig({
   integrations: [
     tailwind(),
     image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })
   ],
   vite: {
     ssr: {
       external: ["svgo"],
     },
+    resolve: {
+      alias: {
+        '~': '/src'
+      }
+    }
   },
 });
